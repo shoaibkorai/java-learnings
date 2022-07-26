a76d4b571c573f39b2af252e0a896283a3d5e02a
Shoaib Korai <107581441+shoaibkorai@users.noreply.github.com>
2022-07-26 11:42:54 +0500

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,10 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        System.out.print("The Sum of The Numbers is : ");
+       // int num1=10;
+        int num2=20;
+        int num1=20;
+        int sum= num1+num2;
+        System.out.println(sum);
+    }
+}
